be14c5c652cf1bbdd47615e8c4d50e0f4e760321
unknown <mayerandras.weblap@gmail.com>
2025-02-03 12:39:51 +0100

    SVG események renderelnek és a belső scroll működik.

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 const {App} = await import(`./src/App.js${app_version}`)
 const e =new App()
+e.events()
--- src/App.js
+++ src/App.js
@@ -6,18 +6,13 @@ export class App{
         const root = document.querySelector("#root")
         root.innerHTML = this.init()
         this.data = [
-            {rowId:0, name:"Első",start:"2025-01-01",end:"2025-01-02",status:0,readyState:0,connections:[]},
-           /*
-            {rowId:1, name:"Második",start:"2025-01-01",end:"2025-01-03",status:0,readyState:20,connections:[]},
-            {rowId:1, name:"Második",start:"2025-01-01",end:"2025-01-04",status:0,readyState:20,connections:[]},
-            {rowId:1, name:"Második",start:"2025-01-01",end:"2025-01-05",status:0,readyState:20,connections:[]},
-            {rowId:1, name:"Második",start:"2025-01-01",end:"2025-01-06",status:0,readyState:20,connections:[]},
-            */
-            //{rowId:2, name:"",start:"",end:"",status:"",readyState:"",connections:[]},
-
+            {rowId:0, name:"Első",start:"2025-01-01",end:"2025-12-31",status:0,readyState:0,connections:[]},
+            {rowId:1, name:"Első",start:"2025-01-03",end:"2025-01-05",status:0,readyState:0,connections:[]},
+            {rowId:2, name:"Első",start:"2025-01-04",end:"2025-01-05",status:0,readyState:0,connections:[]},
+            {rowId:3, name:"Első",start:"2025-01-04",end:"2025-01-12",status:0,readyState:0,connections:[]},
+            {rowId:4, name:"Első",start:"2025-01-06",end:"2025-01-011",status:0,readyState:0,connections:[]},
         ]
         this.render()
-
     }
     calendar(){
         let context = ``
@@ -25,25 +20,36 @@ export class App{
             const color = month % 2 == 0 ? `style="background-color:#ccc"`: ""
             const cal = calendar(`${this.year}-${month}-01`)
             Object.keys(cal).forEach(day=>{
-                //console.log(cal[day])
                 context += `<th ${color} class="cal-day">
                                 <div>${cal[day].monthNameShort}</div>
                                 <div>${cal[day].dayNameShort}</div>
                                 <div>${day}</div>
                             </th>`
-
             })
         }
         return `<table class="SVG_TABLE">
                     <thead>
                         ${context}
                     </thead>
+                    <tbody>
+                        <tr class="svg-table-row"></tr>
+                        <tr class="svg-table-row"></tr>
+                        <tr class="svg-table-row"></tr>
+                        <tr class="svg-table-row"></tr>
+                        <tr class="svg-table-row"></tr>
+                        <tr class="svg-table-row"></tr>
+                        <tr class="svg-table-row"></tr>
+                        <tr class="svg-table-row"></tr>
+                        <tr class="svg-table-row"></tr>
+                        <tr class="svg-table-row"></tr>
+                    <tbody>
                 </table>
-                <svg class="SVG"></svg>
+                <div class="svgCtn">
+                    <svg class="SVG"></svg>
+                </div>
+
                 `
-
     }
-
     init(){
         let context = ``
         for(let i=0;i<10;i++){
@@ -105,17 +111,16 @@ export class App{
         const SVG = document.querySelector(".SVG")
         const SVG_TABLE_THEAD = SVG_TABLE.querySelector("thead").getBoundingClientRect().height 
 
-
-
-        //console.log(this.data)
+        let yPos = 3.5
         this.data.forEach((itm,index)=>{
             const row = document.querySelector(`#row_${itm.rowId}`)
-
-            const diff = dateDiff(itm.start,itm.end)
-
-            console.log(index*20+3)
-            SVG.innerHTML += `<rect x="${5}" y="${index*20+3}" width="${diff*45}" height="${23}" fill="blue" />`
-
+            const evtStartX = dateDiff(`${this.year}-01-01`,itm.start)
+            const evtLength = dateDiff(itm.start,itm.end)
+            
+            //yPos += 1 * index + 5
+            //if(index %2 != 0){ yPos += 14}
+        SVG.innerHTML += `<rect x="${evtStartX*46.5}" y="${index*20+yPos}" width="${evtLength*47+47}" height="${29}" fill="blue" />`
+        yPos += 15
             Object.keys(itm).forEach(key=>{
                 row.querySelector(`.row-${key}`).value = itm[key]
 
@@ -123,20 +128,33 @@ export class App{
 
 
 
-            //console.log(dateDiff(itm.start,itm.end))
         })
 
-
-
-        console.log(SVG_TABLE.querySelector("thead").getBoundingClientRect().height   )
+        this.svgResize()
+    }
+    svgResize(){
+        const DATA = document.querySelector(".DATA")
+        const SVG_TABLE = document.querySelector(".SVG_TABLE")
+        const SVG = document.querySelector(".SVG")
+        const SVG_TABLE_THEAD = SVG_TABLE.querySelector("thead").getBoundingClientRect().height 
 
         DATA.querySelector("thead").style.height = SVG_TABLE_THEAD 
-        SVG_TABLE.style.height = DATA.getBoundingClientRect().height
-        SVG.style.height = DATA.getBoundingClientRect().height - SVG_TABLE_THEAD
-        SVG.style.top =  SVG_TABLE_THEAD + 6
-        //console.log(SVG_TABLE)
+        SVG.style.height = DATA.getBoundingClientRect().height - SVG_TABLE_THEAD - 20
+        SVG.style.top =  SVG_TABLE_THEAD + 8
+              
     }
 
-
-    events(){}
+    events(){
+        window.addEventListener("resize",()=>{
+            this.svgResize()
+        })
+        const element = document.querySelector('.SVG_TABLE')
+        const svgCtn = document.querySelector(".svgCtn")
+        element.onscroll = (e)=>{
+            svgCtn.scrollLeft = element.scrollLeft
+        }
+        svgCtn.onscroll = (e)=>{
+            element.scrollLeft = svgCtn.scrollLeft
+        }
+    }
 }
